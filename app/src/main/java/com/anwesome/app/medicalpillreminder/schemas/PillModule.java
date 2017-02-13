afb312658058153551fe3f1e5613121248c1dee7
Anwesh43 <anweshthecool0@gmail.com>
2017-02-13 14:49:29 +0530

    Adding schemas for the models used by the app

---
+++ app/src/main/java/com/anwesome/app/medicalpillreminder/schemas/PillModule.java
@@ -0,0 +1,13 @@
+package com.anwesome.app.medicalpillreminder.schemas;
+
+import com.anwesome.app.medicalpillreminder.models.Pill;
+
+import io.realm.annotations.RealmModule;
+
+/**
+ * Created by anweshmishra on 12/02/17.
+ */
+@RealmModule(classes = {Pill.class})
+public class PillModule {
+
+}
