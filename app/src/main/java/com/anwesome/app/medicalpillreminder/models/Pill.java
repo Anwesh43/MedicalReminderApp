f56a5f5f44d8222594cdbdbf87ed95f129126583
Anwesh43 <anweshthecool0@gmail.com>
2017-02-13 14:49:11 +0530

    Adding models for the app

---
+++ app/src/main/java/com/anwesome/app/medicalpillreminder/models/Pill.java
@@ -0,0 +1,49 @@
+package com.anwesome.app.medicalpillreminder.models;
+
+import java.util.*;
+
+import io.realm.RealmList;
+import io.realm.RealmObject;
+/**
+ * Created by anweshmishra on 12/02/17.
+ */
+public class Pill extends RealmObject{
+    private String name;
+    private String notificationTimes;
+    private int id;
+    private int pillsNumber = 0;
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setPillsNumber(int pillsNumber) {
+        this.pillsNumber = pillsNumber;
+    }
+    public void changePill(int dir) {
+        this.pillsNumber+=dir;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getPillsNumber() {
+        return pillsNumber;
+    }
+
+    public String getNotificationTimes() {
+        return notificationTimes;
+    }
+
+    public void setNotificationTimes(String notificationTimes) {
+        this.notificationTimes = notificationTimes;
+    }
+    public int hashCode() {
+        return name.hashCode()+id+pillsNumber+notificationTimes.hashCode();
+    }
+}
