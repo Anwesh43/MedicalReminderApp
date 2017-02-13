a2d13c67fcd9e6850ee993e9bb17b28203986b1d
Anwesh43 <anweshthecool0@gmail.com>
2017-02-13 14:50:07 +0530

    Adding MainActivity

---
+++ app/src/main/java/com/anwesome/app/medicalpillreminder/MainActivity.java
@@ -0,0 +1,46 @@
+package com.anwesome.app.medicalpillreminder;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.anwesome.app.medicalpillreminder.buttons.BaseButton;
+import com.anwesome.app.medicalpillreminder.models.Pill;
+import com.anwesome.app.medicalpillreminder.views.MainView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.realm.RealmResults;
+
+public class MainActivity extends AppCompatActivity {
+    MainView mainView;
+    RealmModelUtil realmModelUtil;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mainView =  new MainView(this);
+        List<BaseButton> baseButtons = new ArrayList<BaseButton>() {{
+            add(new BaseButton("Pill Reminder"));
+
+        }};
+        BaseButton pillRefill = new BaseButton("Pill Refill");
+        pillRefill.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,RefillActivity.class);
+                startActivity(intent);
+            }
+        });
+        baseButtons.add(pillRefill);
+        baseButtons.add(new BaseButton("Contact Physician"));
+        mainView.setBaseButtonList(baseButtons);
+        setContentView(mainView);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
+}
