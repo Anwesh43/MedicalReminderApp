afb89d55e4f2f8f202f53d3f24a87ffca33518b1
Anwesh43 <anweshthecool0@gmail.com>
2017-02-17 02:17:09 +0530

    Adding new activity

--- app/src/main/java/com/anwesome/app/medicalpillreminder/ReminderActivity.java
+++ app/src/main/java/com/anwesome/app/medicalpillreminder/ReminderActivity.java
@@ -1,7 +1,20 @@
 package com.anwesome.app.medicalpillreminder;
 
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+
+import com.anwesome.app.medicalpillreminder.views.ReminderLayout;
+
 /**
  * Created by anweshmishra on 17/02/17.
  */
-public class ReminderActivity {
+public class ReminderActivity extends AppCompatActivity {
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        ReminderLayout reminderLayout = new ReminderLayout(this);
+        setContentView(reminderLayout);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Pill Reminder");
+    }
 }
