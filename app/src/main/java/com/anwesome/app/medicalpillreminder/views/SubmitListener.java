0c054798258133a52e0919d2dcb230147d89783b
Anwesh43 <anweshthecool0@gmail.com>
2017-02-20 21:47:41 +0530

    Adding submit listener

---
+++ app/src/main/java/com/anwesome/app/medicalpillreminder/views/SubmitListener.java
@@ -0,0 +1,8 @@
+package com.anwesome.app.medicalpillreminder.views;
+
+/**
+ * Created by anweshmishra on 20/02/17.
+ */
+public interface SubmitListener {
+    void onSubmit(String hour,String minute,String period);
+}
