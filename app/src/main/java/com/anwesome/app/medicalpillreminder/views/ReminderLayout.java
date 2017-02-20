09d234bf4db54e82c418b142b12b6eeb116e0bd6
Anwesh43 <anweshthecool0@gmail.com>
2017-02-20 11:36:37 +0530

    Adding activity variable and view variable

--- app/src/main/java/com/anwesome/app/medicalpillreminder/views/ReminderLayout.java
+++ app/src/main/java/com/anwesome/app/medicalpillreminder/views/ReminderLayout.java
@@ -1,5 +1,6 @@
 package com.anwesome.app.medicalpillreminder.views;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
@@ -12,11 +13,17 @@ import com.anwesome.app.medicalpillreminder.models.Pill;
  * Created by anweshmishra on 17/02/17.
  */
 public class ReminderLayout extends RefillLayout {
+    private SchedulerView schedulerView;
+    private Activity activity;
     public ReminderLayout(Context context, AttributeSet attrs) {
         super(context,attrs);
+        schedulerView = new SchedulerView(context);
+        activity = (Activity)context;
     }
     public ReminderLayout(Context context) {
         super(context);
+        schedulerView = new SchedulerView(context);
+        activity = (Activity)context;
     }
     public boolean plusViewRequired() {
         return false;
@@ -29,16 +36,29 @@ public class ReminderLayout extends RefillLayout {
         private Icon plus;
         public ReminderView(Context context, Pill pill) {
             super(context,pill);
+            this.pill = pill;
         }
         public void init(int w,int h) {
+            plus = new Icon(9*w/10,2*h/3,w/20){
+                public void draw(Canvas c,Paint p) {
+                    super.draw(c,p);
+                    float gap = (this.r*4)/5;
+                    c.drawLine(this.x-gap,this.y,this.x+gap,this.y,p);
+                    c.drawLine(this.x,this.y-gap,this.x,this.y+gap,p);
 
+                }
+                public void handleRealm() {
+                    activity.setContentView(schedulerView);
+                }
+            };
         }
         public void drawControls(Canvas canvas, Paint paint,int w,int h) {
-            String notificationTimes = pill.getNotificationTimes()+"8:30,9:30";
-            canvas.drawText(notificationTimes,w/10,h/6,paint);
+            String notificationTimes = pill.getNotificationTimes()==null?" ":pill.getNotificationTimes()+"8:30,9:30";
+            canvas.drawText(notificationTimes,w/10,2*h/3,paint);
+            plus.draw(canvas,paint);
         }
         public void handleTap(float x,float y) {
-
+            plus.handleTap(x,y);
         }
     }
 }
