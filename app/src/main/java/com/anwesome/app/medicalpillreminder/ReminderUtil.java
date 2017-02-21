7b0c43ff42d3e976890dfe6da3234a547570da2a
Anwesh43 <anweshthecool0@gmail.com>
2017-02-21 14:52:48 +0530

    Correcting the alarm target time issue

--- app/src/main/java/com/anwesome/app/medicalpillreminder/ReminderUtil.java
+++ app/src/main/java/com/anwesome/app/medicalpillreminder/ReminderUtil.java
@@ -3,9 +3,10 @@ package com.anwesome.app.medicalpillreminder;
 import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.provider.CalendarContract;
+import android.os.IBinder;
 
 import com.anwesome.app.medicalpillreminder.models.Pill;
 
@@ -20,6 +21,8 @@ public class ReminderUtil {
         Intent intent = new Intent(activity,NotificationActivity.class);
         intent.putExtra(AppConstants.NAME_EXTRA,pill.getName());
         intent.putExtra(AppConstants.ID_EXTRA,pill.getId());
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
         PendingIntent notificationIntent = PendingIntent.getActivity(activity,0,intent,0);
         AlarmManager alarmManager = (AlarmManager)activity.getSystemService(Context.ALARM_SERVICE);
         int hours = 0;
@@ -27,8 +30,17 @@ public class ReminderUtil {
             hours = 12;
         }
         hours = hours+Integer.parseInt(hour);
+        calendar.setTimeInMillis(System.currentTimeMillis());
         calendar.set(Calendar.HOUR_OF_DAY,hours);
         calendar.set(Calendar.MINUTE,Integer.parseInt(minute));
-        alarmManager.setRepeating(AlarmManager.RTC,AlarmManager.INTERVAL_DAY,calendar.getTimeInMillis(),notificationIntent);
+        alarmManager.setRepeating(AlarmManager.RTC,calendar.getTimeInMillis(),5*60*1000,notificationIntent);
+    }
+    class ReminderService extends Service {
+        public void onCreate() {
+
+        }
+        public IBinder onBind(Intent intent) {
+            return null;
+        }
     }
 }
